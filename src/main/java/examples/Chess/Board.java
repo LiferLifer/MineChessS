2d7a2818203395ac5ca41496deaff0fdf50f2101
LiferLifer <12112748@mail.sustech.edu.cn>
2022-05-20 18:20:54 +0800

    改了一下棋子基本属性，走棋待修改。

--- src/main/java/examples/Chess/Board.java
+++ src/main/java/examples/Chess/Board.java
@@ -16,33 +16,28 @@ public class Board extends BaseBoard {
             }
         }
 
-        Piece.PieceType bottomLine[] = new Piece.PieceType[]{
-                Piece.PieceType.JU,
-                Piece.PieceType.MA,
-                Piece.PieceType.XIANG,
-                Piece.PieceType.SHI,
-                Piece.PieceType.SHUAI,
-                Piece.PieceType.SHI,
-                Piece.PieceType.XIANG,
-                Piece.PieceType.MA,
-                Piece.PieceType.JU
+        Piece.PieceType[] bottomLine = new Piece.PieceType[]{
+                Piece.PieceType.R,
+                Piece.PieceType.N,
+                Piece.PieceType.B,
+                Piece.PieceType.Q,
+                Piece.PieceType.K,
+                Piece.PieceType.B,
+                Piece.PieceType.N,
+                Piece.PieceType.R
         };
-        for (int i = 0; i < 9; i++) {
-            grids[i][0].setOwnedPiece(new Piece(i, 0, bottomLine[i], Color.RED));
+        for (int i = 0; i < 8; i++) {
+            grids[i][0].setOwnedPiece(new Piece(i, 0, bottomLine[i], Color.WHITE));
         }
-        for (int i = 0; i < 9; i++) {
-            grids[i][9].setOwnedPiece(new Piece(i, 9, bottomLine[i], Color.BLACK));
+        for (int i = 7; i >=0 ; i--) {
+            grids[i][7].setOwnedPiece(new Piece(i, 7, bottomLine[i], Color.BLACK));
         }
 
-        grids[1][2].setOwnedPiece(new Piece(1, 2, Piece.PieceType.PAO, Color.RED));
-        grids[7][2].setOwnedPiece(new Piece(7, 2, Piece.PieceType.PAO, Color.RED));
-        grids[1][7].setOwnedPiece(new Piece(1, 7, Piece.PieceType.PAO, Color.BLACK));
-        grids[7][7].setOwnedPiece(new Piece(7, 7, Piece.PieceType.PAO, Color.BLACK));
-        for (int i = 0; i < 9; i += 2) {
-            grids[i][3].setOwnedPiece(new Piece(i, 3, Piece.PieceType.BING, Color.RED));
+        for (int i = 0; i < 8; i ++) {
+            grids[i][1].setOwnedPiece(new Piece(i, 1, Piece.PieceType.P, Color.WHITE));
         }
-        for (int i = 0; i < 9; i += 2) {
-            grids[i][6].setOwnedPiece(new Piece(i, 6, Piece.PieceType.BING, Color.BLACK));
+        for (int i = 0; i < 8; i ++) {
+            grids[i][6].setOwnedPiece(new Piece(i, 6, Piece.PieceType.P, Color.BLACK));
         }
     }
 }
